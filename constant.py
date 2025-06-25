a38106107071b6a9673856daf0430fa01a941e16
grechiiii <a20234861@pucp.edu.pe>
2025-06-25 02:18:33 -0500

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,30 @@
+info = {
+   "Pronoun": "ella", 
+   "Name": "Grecia",
+   "Full_Name": "Grecia García Hoyos",
+   "Intro": "Estudiante de Publicidad y apasionada por la comunicación creativa con impacto social",
+   "About": """Hola, soy Grecia García Hoyos, estudiante de Publicidad en la PUCP. Me destaco por ser una persona productiva, puntual y con habilidades de liderazgo y organización. 
+Siempre busco aprender, colaborar y crear impacto desde la comunicación.""",
+   "Tableau": "",  # Puedes dejarlo vacío si no tienes uno
+   "Medium": "https://www.linkedin.com/in/grecia-garcia-hoyos-44997933a/",  # Puedes cambiarlo por otro link si prefieres
+   "City": "Lima, Perú",
+   "Photo":"""<a href="https://www.linkedin.com/in/grecia-garcia-hoyos-44997933a/"><img src="https://i.imgur.com/iLMPjxN.jpeg" width="200" alt="Grecia García Hoyos" title="Grecia García Hoyos"></a>""",
+   "Email": "a20234861@pucp.edu.pe"  # No olvides reemplazar por tu correo real
+}
+
+embed_rss = {
+    'rss': """<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
+        data-rss="https://medium.com/feed/@vicky-note"
+        data-maxcols="3" 
+        data-layout="grid"
+        data-poststyle="inline" 
+        data-readmore="Read the rest" 
+        data-buttonclass="btn btn-primary" 
+        data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+}
+
+endorsements = {
+    "img1": "https://i.imgur.com/YQx2CP1.jpeg",
+    "img2": "https://i.imgur.com/CmdHRw2.jpeg",
+    "img3": "https://i.imgur.com/jakXIXZ.jpeg"
+}
